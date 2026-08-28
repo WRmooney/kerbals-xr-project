a02b95fa624c37fbe90f278df022b61168d54fe6
Wseiders <wseiders@mit.edu>
2026-08-28 14:02:01 -0400

    Revert "Conflict Test"
    This reverts commit 66bc4cce8328e26f46611de8c46b680ac3f95af1.

--- build_snap_grid.gd
+++ build_snap_grid.gd
@@ -3,10 +3,6 @@ extends Node3D
 @onready var area_3d_2: Area3D = $Area3D2
 @onready var area_3d_3: Area3D = $Area3D3
 @onready var area_3d_4: Area3D = $Area3D4
-@onready var body: RigidBody3D = $"."
-@onready var thruster: Node3D = $Root
-@onready var tip: Node3D = $"."
-
 
 
 # Called when the node enters the scene tree for the first time.
