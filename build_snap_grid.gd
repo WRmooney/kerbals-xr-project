ca30285ef4a29e195ef49cff6ab9659e7392c598
Wseiders <wseiders@mit.edu>
2026-08-28 11:04:21 -0400

    table

--- build_snap_grid.gd
+++ build_snap_grid.gd
@@ -1,25 +1,25 @@
 extends Node3D
 @onready var area_3d: Area3D = $Area3D
 @onready var area_3d_2: Area3D = $Area3D2
+@onready var area_3d_3: Area3D = $Area3D3
+@onready var area_3d_4: Area3D = $Area3D4
 
 
 # Called when the node enters the scene tree for the first time.
 
 
 
-func _on_area_3d_body_entered(body):
+func _on_area_3d_body_entered():
 	body.global_transform = area_3d.global_transform
 
 
-func _on_area_3d_2_body_entered(body: Node3D) -> void:
-	body.global_transform = area_3d_2.global_transform
+func _on_area_3d_2_body_entered():
+	tip.global_transform = area_3d_2.global_transform
 
 
-func _on_area_3d_3_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+func _on_area_3d_3_body_entered():
+	thruster.global_transform = area_3d_3.global_transform
 
 
-
-
-func _on_area_3d_4_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+func _on_area_3d_4_body_entered():
+	fin.global_transform = area_3d_4.global_transform
